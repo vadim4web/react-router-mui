a4e47aebbe3deadfa29cf73a1df013ab3684a394
1abcdesign <one.abcdesign@gmail.com>
2024-11-13 14:40:19 +0200

    Updates

--- assets/DummyComponent-81ddf250.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./index-0d4ae6e3.js";function u(e,n){typeof e=="function"?e(n):e&&(e.current=n)}const f=typeof window<"u"?t.useLayoutEffect:t.useEffect,s=f;function r(...e){return t.useMemo(()=>e.every(n=>n==null)?null:n=>{e.forEach(o=>{u(o,n)})},e)}export{s as a,u as s,r as u};
