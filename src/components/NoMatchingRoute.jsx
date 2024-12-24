f1effa36b496afdbd423eefb5a1449bc85dda911
vadim4web <vadim4web@gmail.com>
2024-12-24 11:08:30 +0200

    NoMatchingRoute: back link to button back

--- src/components/NoMatchingRoute.jsx
+++ src/components/NoMatchingRoute.jsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useContext } from 'react';
 import { AppContext } from '../AppContext';
 import computeHeight from '../utils/computeHeight';
