2441999752add2fabff0dfe66cb279bfb0e87696
vadim4web <vadim4web@gmail.com>
2024-12-24 11:42:23 +0200

    Updates

---
+++ assets/DummyComponent-a1d9f9bb.js
@@ -0,0 +1,1 @@
+import{r as t}from"./index-a4e6dee1.js";function u(e,n){typeof e=="function"?e(n):e&&(e.current=n)}const f=typeof window<"u"?t.useLayoutEffect:t.useEffect,s=f;function r(...e){return t.useMemo(()=>e.every(n=>n==null)?null:n=>{e.forEach(o=>{u(o,n)})},e)}export{s as a,u as s,r as u};
