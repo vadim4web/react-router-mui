9ea6e1e8b2739536b61c6727468ee002263c6d9a
vadim4web <vadim4web@gmail.com>
2024-12-24 11:08:03 +0200

    Updates

---
+++ assets/DummyComponent-2f38f1ec.js
@@ -0,0 +1,1 @@
+import{r as t}from"./index-e4b91322.js";function u(e,n){typeof e=="function"?e(n):e&&(e.current=n)}const f=typeof window<"u"?t.useLayoutEffect:t.useEffect,s=f;function r(...e){return t.useMemo(()=>e.every(n=>n==null)?null:n=>{e.forEach(o=>{u(o,n)})},e)}export{s as a,u as s,r as u};
--- assets/DummyComponent-6ae2a449.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./index-69f9a04e.js";function u(e,n){typeof e=="function"?e(n):e&&(e.current=n)}const f=typeof window<"u"?t.useLayoutEffect:t.useEffect,s=f;function r(...e){return t.useMemo(()=>e.every(n=>n==null)?null:n=>{e.forEach(o=>{u(o,n)})},e)}export{s as a,u as s,r as u};
