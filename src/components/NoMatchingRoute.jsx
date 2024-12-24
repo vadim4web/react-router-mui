a7cc35305be180dc996c578e62d8d9c28687e3e9
vadim4web <vadim4web@gmail.com>
2024-12-24 11:07:25 +0200

    NoMatchingRoute: back link to button back

--- src/components/NoMatchingRoute.jsx
+++ src/components/NoMatchingRoute.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useContext } from 'react';
 import { AppContext } from '../AppContext';
 import computeHeight from '../utils/computeHeight';
@@ -6,6 +6,7 @@ import './NoMatchingRoute.css';
 
 function NoMatchingRoute() {
   const { showFrame, isLandscape } = useContext(AppContext);
+  const navigate = useNavigate();
 
   return (
     <div
@@ -13,8 +14,9 @@ function NoMatchingRoute() {
       style={computeHeight(showFrame, isLandscape, '40.5dvh', '43.5dvh', '73.5dvh', '76.5dvh')}
     >
       <h2>Nothing to see here!</h2>
-      <p className="back-to-start">
-        <Link to="/dummyTable">Go to the starting page</Link>
+      <p className="back-link">
+        {/* Navigate to the previous page */}
+        <button onClick={() => navigate(-1)}>Go Back</button>
       </p>
     </div>
   );
